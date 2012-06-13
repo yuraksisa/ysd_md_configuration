43c57911c74a8b613beea2a930885cb3b3711bd5
yurak <yurak.sisa.dream@gmail.com>
2012-06-13 12:39:50 +0200

    Variable refactoring

--- lib/ysd_md_variable.rb
+++ lib/ysd_md_variable.rb
@@ -12,5 +12,28 @@ module SystemConfiguration
     property :module, String, :field => 'module', :length => 64 # Which defines the variable
     property :description, String, :field => 'description', :length => 256 # The variable description
     
+    #
+    # Gets the value of the variable
+    #
+    # @param [String] name
+    #   The variable name
+    #
+    # @param [Object] default_value
+    #
+    # @return [String] value
+    #   The variable value or the default value if the variable is not defined
+    #
+    def self.get_value(name, default_value=nil)
+    
+      sysvar = Variable.get(name)
+    
+      return_value = if sysvar
+                       sysvar.value
+                     else
+                       default_value
+                     end      
+    
+    end
+        
   end #Variable
 end #SystemConfiguration

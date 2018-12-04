77a33f76cd125243715389f681cc6ebde7d6edc1
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2018-12-04 22:44:15 +0100

    counter models

--- lib/ysd_md_counter.rb
+++ lib/ysd_md_counter.rb
@@ -13,6 +13,50 @@ module SystemConfiguration
     property :name, String, length: 128
     property :serie, String, length: 10
     property :value, Integer, default: 0
+    
+    #
+    # Get the series
+    #
+    def self.series(name)
+   
+       query = <<-SQL
+                select distinct(serie) from conf_counters where name = ?
+              SQL
+       repository.adapter.select(query, name)
+
+    end 	
+
+    #
+    # Get the next value
+    #
+    def self.next_value(name, serie)
+      if counter = get_counter(name, serie)
+        next_value = counter.value + 1
+        counter.update(value: next_value)
+        return next_value
+      end
+    end 	
+
+    #
+    # Get a counter by its name and serie
+    #
+    def self.get_counter(name, serie)
+      if serie.nil? or serie.empty?	
+        SystemConfiguration::Counter.first(name: name, serie: nil)
+      else
+      	SystemConfiguration::Counter.first(name: name, serie: serie)
+      end  
+    end
+    
+    #
+    # Set a counter value
+    #
+    def self.set_counter(name, serie, value)
+      if counter = get_counter(name, serie)
+        counter.update(value)
+      end
+    end 	
+
 
   end
 end
--- ysd_md_configuration.gemspec
+++ ysd_md_configuration.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name    = "ysd_md_configuration"
-  s.version = "0.2.9"
+  s.version = "0.2.10"
   s.authors = ["Yurak Sisa Dream"]
   s.date    = "2012-05-09"
   s.email   = ["yurak.sisa.dream@gmail.com"]
